a15d3d3e270efebf40426ce92d30a04aa0dcecbf
Brian Wilcove <99514411+Brian-Wilcove@users.noreply.github.com>
2023-12-20 15:06:15 -0500

    Created routes model folder & edited the post route as a test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const express = require('express')
-const workoutRoutes = require('./routes/workout')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-// listener
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listener
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
